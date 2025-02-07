dc0c89ae765e9b956338a80cb713c4a53ba297fb
Marcus Souza <marcusricardo2703@gmail.com>
2025-02-07 12:04:29 -0300

    Atualizao - Estrutura HTML, CSS e JS

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -8,28 +8,33 @@ function adicionarNome() {
         nomes.push(nome);
         nomeInput.value = ""; // Limpa o campo após adicionar
         console.log(nomes);
-        //renderizarNomes();
+        atualizarListaParticipantes();
     }
 }
 
-//function removerNome(nome) {
-//    nomes = nomes.filter(item => item !== nome);
-//    renderizarNomes();
-//}
+function atualizarListaParticipantes() {
+    const listaDiv = document.getElementById('lista-participantes');
+    listaDiv.innerHTML = ''; // Limpa a lista atual
+    
+    if (nomes.length === 0) {
+        listaDiv.style.display = 'none'; // Torna a div invisível se não houver nomes
+    } else {
+        listaDiv.style.display = 'block'; // Torna a div visível se houver nomes
+        nomes.forEach(nome => {
+            const p = document.createElement('p');
+            p.textContent = nome;
+            listaDiv.appendChild(p);
+        });
+    }
+}
 
-//function renderizarNomes() {
-//    const listaNomes = document.getElementById('listaNomes');
-//    listaNomes.innerHTML = ""; // Limpa a lista
-
-//    nomes.forEach(nome => {
-//        const nomeCaixa = document.createElement('div');
-//        nomeCaixa.classList.add('nomeCaixa');
-        
-//        nomeCaixa.innerHTML = `
-//            ${nome} 
-//            <button onclick="removerNome('${nome}')">X</button>
-//        `;
-        
-//        listaNomes.appendChild(nomeCaixa);
-//    });
-//}
+function sortearNome() {
+    if (nomes.length === 0) {
+        alert("A lista de participantes está vazia.");
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * nomes.length);
+    const nomeSorteado = nomes[indiceAleatorio];
+    alert("O nome sorteado é: " + nomeSorteado);
+}
